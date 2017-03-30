bd798f3ff790fb74f0c086588c64839045156149
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-03-31 01:52:04 +0530

    Update: 100% test coverage

--- test/data-structures/testLinkedList.js
+++ test/data-structures/testLinkedList.js
@@ -53,5 +53,72 @@ describe('LinkedList', () => {
     inst.push(2);
     const listString = inst.toString();
     assert.equal(listString, '2 -> 1 -> null');
-  });  
+  });
+
+  it('should call function on all nodes', () => {
+    const inst = new LinkedList();
+
+    inst.push(1);
+    inst.push(2);
+
+    inst.forEach(value => 2 * value);
+    const listString = inst.toString();
+    assert.equal(listString, '4 -> 2 -> null');
+  });
+
+
+  it('should return list as array', () => {
+    const inst = new LinkedList();
+
+    inst.push(1);
+    inst.push(2);
+    const listArray = inst.getListAsArray();
+    assert.equal(listArray[0], 2);
+    assert.equal(listArray[1], 1);
+  });
+
+
+  it('should return first node that matches', () => {
+    const inst = new LinkedList();
+
+    inst.push({ t: 1 });
+    inst.push({ x: 2 });
+    const node = inst.getNodeByValue({ t: 1 });
+    assert.deepStrictEqual(node.value, { t: 1 });
+  });
+
+  it('should return -1 if node not listed', () => {
+    const inst = new LinkedList();
+
+    inst.push({ t: 1 });
+    inst.push({ x: 2 });
+    const node = inst.getNodeByValue({ t: 2 });
+    assert.deepStrictEqual(node, -1);
+  });
+
+  it('should return node at index', () => {
+    const inst = new LinkedList();
+
+    inst.push({ t: 1 });
+    inst.push({ x: 2 });
+    const node = inst.getNodeByIndex(1);
+    assert.deepStrictEqual(node.value, { t: 1 });
+  });
+
+  it('should throw error if index out of bound while getting node', () => {
+    const inst = new LinkedList();
+
+    inst.push({ t: 1 });
+    inst.push({ x: 2 });
+    assert.throws(() => inst.getNodeByIndex(5), Error);
+  });
+
+
+  it('should throw error if index out of bound while popping', () => {
+    const inst = new LinkedList();
+
+    inst.push({ t: 1 });
+    inst.push({ x: 2 });
+    assert.throws(() => inst.popNodeByIndex(5), Error);
+  });
 });
