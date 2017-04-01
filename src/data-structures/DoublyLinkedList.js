3a3a35b06867a521a829b80100c307c23555963b
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-04-01 23:26:44 +0530

    DoublyLinkedList: Initialized

---
+++ src/data-structures/DoublyLinkedList.js
@@ -0,0 +1,28 @@
+const Node = require('./Node');
+const deepStrictEqual = require('assert').deepStrictEqual;
+
+class DoublyLinkedList {
+  constructor() {
+    this._head = null;
+    this._tail = null;
+    this._length = 0;
+  }
+
+  /**
+   * Get length of Doubly Linked List
+   * @return {int} Length of Linked List
+   */
+  get length() {
+    return this._length;
+  }
+
+  /**
+   * Check if Doubly Linked List is empty or not
+   * @return {Boolean} `true` if empty else `false`
+   */
+  isEmpty() {
+    return this._length === 0;
+  }
+}
+
+module.exports = DoublyLinkedList;
