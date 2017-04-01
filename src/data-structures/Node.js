de9e4ead5af9ce1976476fa60e846e202963172c
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-04-01 23:22:13 +0530

    Node: Add prev option

--- src/data-structures/Node.js
+++ src/data-structures/Node.js
@@ -2,6 +2,7 @@ class Node {
   constructor(data) {
     this._value = data;
     this._next = null;
+    this._prev = null;
   }
 
   get value() {
@@ -12,12 +13,20 @@ class Node {
     return this._next;
   }
 
+  get prev() {
+    return this._prev;
+  }
+
   set value(data) {
     this._value = data;
   }
 
   set next(node) {
     this._next = node;
+  }
+
+  set prev(node) {
+    this._prev = node;
   }
 }
 
