895a59456addea4b43cf283ab9668747a615e392
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-04-02 22:32:14 +0530

    DoublyLinkedList: Test push to the front of the list

--- test/data-structures/testDoublyLinkedList.js
+++ test/data-structures/testDoublyLinkedList.js
@@ -8,4 +8,17 @@ describe('DoublyLinkedList', () => {
     assert(inst.isEmpty());
     assert.equal(inst.length, 0);
   });
+
+  it('should push node to the front', () => {
+    const inst = new DoublyLinkedList();
+    assert(inst.isEmpty());
+
+    inst.push(1);
+    assert.equal(inst.length, 1);
+
+    inst.push(2);
+    assert.equal(inst.length, 2);
+
+    assert.equal(inst.toString(), '2 -> 1 -> null');
+  });
 });
