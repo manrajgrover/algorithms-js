00edb2d17858801c87273513637d57f31dd7b6a7
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-04-02 22:26:57 +0530

    DoublyLinkedList: Add push method

--- src/data-structures/DoublyLinkedList.js
+++ src/data-structures/DoublyLinkedList.js
@@ -23,6 +23,25 @@ class DoublyLinkedList {
   isEmpty() {
     return this._length === 0;
   }
+
+  /**
+   * Pushes node to the front of Linked List
+   * @param  {*} data Data or value contained in Node
+   * @return {None}
+   */
+  push(data) {
+    const newNode = new Node(data);
+
+    newNode.next = this._head;
+    newNode.prev = null;
+
+    if (this._head !== null) {
+      this._head.prev = newNode;
+    }
+
+    this._head = newNode;
+    this._length += 1;
+  }
 }
 
 module.exports = DoublyLinkedList;
