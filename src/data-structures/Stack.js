7f2d300f34fd635bc531bebbfae948841b5e8cca
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-04-08 22:52:37 +0530

    Stack: Add isEmpty method

--- src/data-structures/Stack.js
+++ src/data-structures/Stack.js
@@ -12,6 +12,15 @@ class Stack {
   get size() {
     return this._list.length;
   }
+
+  /**
+   * Check if Stack is empty or not
+   * @return {Boolean} `true` if empty else `false`
+   */
+  isEmpty() {
+    return this._list.isEmpty();
+  }
+
 }
 
 module.exports = Stack;
