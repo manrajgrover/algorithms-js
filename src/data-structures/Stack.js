896d2cabc303ae9506ff1fac70de25f293bfb0f5
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-04-08 22:52:11 +0530

    Stack: Add size method to Stack

--- src/data-structures/Stack.js
+++ src/data-structures/Stack.js
@@ -4,6 +4,14 @@ class Stack {
   constructor() {
     this._list = new DoublyLinkedList();
   }
+
+  /**
+   * Get length of Stack
+   * @return {Number} Size of Stack
+   */
+  get size() {
+    return this._list.length;
+  }
 }
 
 module.exports = Stack;
