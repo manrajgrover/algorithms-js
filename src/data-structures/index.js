2a82761f82f323fd19fba9c3936556559ef263cf
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-04-08 22:51:35 +0530

    Stack: Import for Stack updated

--- src/data-structures/index.js
+++ src/data-structures/index.js
@@ -1,9 +1,11 @@
 const DoublyLinkedList = require('./DoublyLinkedList');
 const LinkedList = require('./LinkedList');
 const Node = require('./Node');
+const Stack = require('./Stack');
 
 module.exports = {
   DoublyLinkedList,
   LinkedList,
-  Node
+  Node,
+  Stack
 };
