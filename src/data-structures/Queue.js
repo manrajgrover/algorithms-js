75e2c35655e81e621a9ba3aa999f4a7948881a1a
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-04-09 00:18:34 +0530

    Queue: Add size method

--- src/data-structures/Queue.js
+++ src/data-structures/Queue.js
@@ -4,6 +4,15 @@ class Queue {
   constructor() {
     this._list = new DoublyLinkedList();
   }
+
+  /**
+   * Get length of Queue
+   * @return {Number} Size of Queue
+   */
+  get size() {
+    return this._list.length;
+  }
+
 }
 
 module.exports = Queue;
