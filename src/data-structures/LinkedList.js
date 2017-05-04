d43e180d75730a4a4eba8323ce189fd78542658d
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-05-04 23:50:18 +0530

    Node: Moves node to separate file

--- src/data-structures/LinkedList.js
+++ src/data-structures/LinkedList.js
@@ -1,5 +1,27 @@
-const Node = require('./Node');
 const deepStrictEqual = require('assert').deepStrictEqual;
+
+class Node {
+  constructor(data) {
+    this._value = data;
+    this._next = null;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  get next() {
+    return this._next;
+  }
+
+  set value(data) {
+    this._value = data;
+  }
+
+  set next(node) {
+    this._next = node;
+  }
+}
 
 class LinkedList {
   constructor() {
