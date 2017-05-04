0f08843d12fce2e700f80d9e89c9af374d28754c
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-05-04 23:56:51 +0530

    Node: Remove from indexjs

--- src/data-structures/index.js
+++ src/data-structures/index.js
@@ -1,13 +1,11 @@
 const DoublyLinkedList = require('./DoublyLinkedList');
 const LinkedList = require('./LinkedList');
-const Node = require('./Node');
 const Queue = require('./Queue');
 const Stack = require('./Stack');
 
 module.exports = {
   DoublyLinkedList,
   LinkedList,
-  Node,
   Queue,
   Stack
 };
