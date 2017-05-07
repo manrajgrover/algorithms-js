b9f441b5a2112a7bc31833e77a5936df1e8eb369
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-05-07 23:45:21 +0530

    Heap: Initialized

---
+++ src/data-structures/Heap.js
@@ -0,0 +1,45 @@
+class Heap {
+  constructor(compareFunction) {
+    this._list = [];
+    this._length = 0;
+    this._compareFunction = compareFunction;
+  }
+
+  get size() {
+    return this._length;
+  }
+
+  _parent(index) {
+    return (index - 1) / 2;
+  }
+
+  _left(index) {
+    return (2 * index) + 1;
+  }
+
+  _right(index) {
+    return (2 * index) + 2;
+  }
+
+  _swap(x, y) {
+    [this._list[x], this._list[y]] = [this._list[y], this._list[x]];
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  push(element) {
+    this._length += 1;
+    this._list.push(element);
+
+    let elemIndex = this._length - 1;
+
+    while (elemIndex !== 0 && this._list[this._parent(elemIndex)] > this._list[elemIndex]) {
+      this._swap(this._parent(elemIndex), elemIndex);
+      elemIndex = this._parent(elemIndex);
+    }
+  }
+}
+
+module.exports = Heap;
--- src/data-structures/index.js
+++ src/data-structures/index.js
@@ -1,10 +1,12 @@
 const DoublyLinkedList = require('./DoublyLinkedList');
+const Heap = require('./Heap');
 const LinkedList = require('./LinkedList');
 const Queue = require('./Queue');
 const Stack = require('./Stack');
 
 module.exports = {
   DoublyLinkedList,
+  Heap,
   LinkedList,
   Queue,
   Stack
