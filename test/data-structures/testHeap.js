59e8bf2382acfc97deacaa0abf37da0823c551fc
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-05-08 23:41:08 +0530

    Heap: Tests insert method

--- test/data-structures/testHeap.js
+++ test/data-structures/testHeap.js
@@ -8,4 +8,18 @@ describe('Heap', () => {
     assert(inst.isEmpty());
     assert.equal(inst.size, 0);
   });
+
+  it('should insert values into heap', () => {
+    const inst = new Heap((a, b) => a < b);
+    assert(inst.isEmpty());
+    assert.equal(inst.size, 0);
+
+    inst.push(5);
+    inst.push(20);
+    inst.push(30);
+    inst.push(10);
+
+    assert.equal(inst.top(), 5);
+    assert.equal(inst.size, 4);
+  });
 });
