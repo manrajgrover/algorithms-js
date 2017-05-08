b78a41de03576ba4057069be3f338171c4889978
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-05-08 23:41:19 +0530

    Heap: Tests pop method

--- test/data-structures/testHeap.js
+++ test/data-structures/testHeap.js
@@ -22,4 +22,37 @@ describe('Heap', () => {
     assert.equal(inst.top(), 5);
     assert.equal(inst.size, 4);
   });
+
+  it('should pop values from heap', () => {
+    const inst = new Heap((a, b) => a < b);
+    assert(inst.isEmpty());
+    assert.equal(inst.size, 0);
+
+    inst.push(5);
+    inst.push(30);
+    inst.push(10);
+    inst.push(20);
+    inst.push(100);
+
+    assert.equal(inst.top(), 5);
+    assert.equal(inst.size, 5);
+
+    inst.pop();
+    inst.pop();
+    assert.equal(inst.top(), 20);
+    assert.equal(inst.size, 3);
+
+    inst.pop();
+    inst.pop();
+    assert.equal(inst.top(), 100);
+    assert.equal(inst.size, 1);
+
+    inst.pop();
+    assert.equal(inst.top(), null);
+    assert.throws(() => inst.pop(), Error);
+
+    inst.push(10);
+    assert.equal(inst.top(), 10);
+    assert.equal(inst.size, 1);
+  });
 });
