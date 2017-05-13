a0b522d1d30af4e67c29e6b4a77ea576401a0187
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-05-14 01:39:33 +0530

    Test Bubble Sort: Test the sorting method

--- test/algorithms/sorting/testBubbleSort.js
+++ test/algorithms/sorting/testBubbleSort.js
@@ -7,4 +7,13 @@ describe('BubbleSort', () => {
     const inst = new BubbleSort();
     assert.equal(inst.size, 0);
   });
+
+  it('should sort the array', () => {
+    const inst = new BubbleSort([2, 1, 3, 4]);
+    assert.equal(inst.size, 4);
+
+    assert.deepEqual(inst.unsortedList, [2, 1, 3, 4]);
+    assert.deepEqual(inst.sortedList, [1, 2, 3, 4]);
+    assert.equal(inst.toString(), '1, 2, 3, 4');
+  });
 });
