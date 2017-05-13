3febb19747c17d4d9a546c689c946a1e36ebef2f
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-05-14 01:37:46 +0530

    BubbleSort: Maintain unsorted array

--- src/algorithms/sorting/BubbleSort.js
+++ src/algorithms/sorting/BubbleSort.js
@@ -1,7 +1,10 @@
 class BubbleSort {
   constructor(data = [], compareFunc = (a, b) => a < b) {
-    this._list = data;
+    this._unsortedList = data;
     this._compareFunc = compareFunc;
+
+    this._sortedList = this._sort(data.slice());
+    this._length = data.length;
   }
 
   get size() {
