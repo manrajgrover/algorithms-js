8308966374a0aeb887dd3d3b4081cb3ba2ab65bc
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-05-14 01:36:08 +0530

    BubbleSort: Sort added

--- src/algorithms/sorting/BubbleSort.js
+++ src/algorithms/sorting/BubbleSort.js
@@ -8,8 +8,27 @@ class BubbleSort {
     return this._list.length;
   }
 
-  sort() {
-    const data = this._list;
+  _sort(list) {
+    const len = list.length;
+    let didSwap;
+
+    for (let i = 0; i < len - 1; i += 1) {
+      didSwap = false;
+
+      for (let j = 0; j < len - i - 1; j += 1) {
+        if (this._compareFunc(list[j + 1], list[j])) {
+          [list[j], list[j + 1]] = [list[j + 1], list[j]];
+          didSwap = true;
+        }
+      }
+
+      if (!didSwap) {
+        break;
+      }
+    }
+
+    return list;
+  }
 
     data.forEach((val, index, array) => {
       console.log(val, index, array);
