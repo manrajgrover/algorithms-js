8136efb0a9fadd51ca857b9851880e93af8e3e22
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-05-15 23:19:52 +0530

    Selection Sort: Initialised

---
+++ src/algorithms/sorting/SelectionSort.js
@@ -0,0 +1,11 @@
+class SelectionSort {
+  constructor(data = [], compareFunc = (a, b) => a < b) {
+    this._unsortedList = data;
+    this._compareFunc = compareFunc;
+
+    this._sortedList = this._sort(data.slice());
+    this._length = data.length;
+  }
+}
+
+module.exports = SelectionSort;
