b7ce152994ea7add667862e0b8a5c503903ef63a
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-05-15 23:03:48 +0530

    Test Bubble Sort: Test for small arrays

--- test/algorithms/sorting/testBubbleSort.js
+++ test/algorithms/sorting/testBubbleSort.js
@@ -18,4 +18,22 @@ describe('BubbleSort', () => {
     assert.deepEqual(inst.sortedList, [1, 2, 3, 4]);
     assert.equal(inst.toString(), '1, 2, 3, 4');
   });
+
+  it('should sort 2 element array', () => {
+    const inst = new BubbleSort([2, 1]);
+    assert.equal(inst.size, 2);
+
+    assert.deepEqual(inst.unsortedList, [2, 1]);
+    assert.deepEqual(inst.sortedList, [1, 2]);
+    assert.equal(inst.toString(), '1, 2');
+  });
+
+  it('should sort 1 element array', () => {
+    const inst = new BubbleSort([1]);
+    assert.equal(inst.size, 1);
+
+    assert.deepEqual(inst.unsortedList, [1]);
+    assert.deepEqual(inst.sortedList, [1]);
+    assert.equal(inst.toString(), '1');
+  });
 });
