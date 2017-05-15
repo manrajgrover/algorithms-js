4de174bbaa650ba5a7608808a6d2695013c46c99
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-05-15 23:53:36 +0530

    Selection Sort: Getters and Setters

--- src/algorithms/sorting/SelectionSort.js
+++ src/algorithms/sorting/SelectionSort.js
@@ -6,6 +6,18 @@ class SelectionSort {
     this._sortedList = this._sort(data.slice());
     this._length = data.length;
   }
+
+  get size() {
+    return this._length;
+  }
+
+  get unsortedList() {
+    return this._unsortedList;
+  }
+
+  get sortedList() {
+    return this._sortedList;
+  }
 }
 
 module.exports = SelectionSort;
