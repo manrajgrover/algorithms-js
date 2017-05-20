a68e7866d6471da0b62690541c05a1b480d2c2d1
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-05-20 22:43:09 +0530

    Insertion Sort: Getters method added

--- src/algorithms/sorting/InsertionSort.js
+++ src/algorithms/sorting/InsertionSort.js
@@ -6,6 +6,31 @@ class InsertionSort {
     this._sortedList = this._sort(data.slice());
     this._length = data.length;
   }
+
+  /**
+   * Get size of array
+   * @return {Number} Size of array
+   */
+  get size() {
+    return this._length;
+  }
+
+  /**
+   * Get unsorted array
+   * @return {Array} Unsorted/Initial array
+   */
+  get unsortedList() {
+    return this._unsortedList;
+  }
+
+  /**
+   * Get sorted array
+   * @return {Array} Sorted array
+   */
+  get sortedList() {
+    return this._sortedList;
+  }
+
 }
 
 module.exports = InsertionSort;
