b38cae7323a77cb488f4404df0885eb2c5f44c99
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-05-20 23:01:32 +0530

    Test Insertion Sort: Test for sorting in ascending order with equal values

--- test/algorithms/sorting/testInsertionSort.js
+++ test/algorithms/sorting/testInsertionSort.js
@@ -19,4 +19,13 @@ describe('Insertion Sort', () => {
     assert.equal(inst.toString(), '1, 2, 3, 4');
   });
 
+  it('should sort the array in ascending order with few equal vals', () => {
+    const inst = new InsertionSort([2, 1, 3, 4, 2], (a, b) => a < b);
+    assert.equal(inst.size, 5);
+
+    assert.deepEqual(inst.unsortedList, [2, 1, 3, 4, 2]);
+    assert.deepEqual(inst.sortedList, [1, 2, 2, 3, 4]);
+    assert.equal(inst.toString(), '1, 2, 2, 3, 4');
+  });
+
 });
