77f6580ff3cbd05eda3892198695221e51e23d9c
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-05-20 22:43:27 +0530

    Insertion Sort: Sorting method implemented

--- src/algorithms/sorting/InsertionSort.js
+++ src/algorithms/sorting/InsertionSort.js
@@ -31,6 +31,28 @@ class InsertionSort {
     return this._sortedList;
   }
 
+  /**
+   * Insertion Sorts the array
+   * @param  {Array} list Array to be sorted
+   * @return {Array}      Sorted array
+   */
+  _sort(list) {
+    const len = list.length;
+
+    for (let i = 1; i < len; i += 1) {
+      let j = i - 1;
+      const key = list[i];
+
+      while (j >= 0 && this._compareFunc(key, list[j])) {
+        list[j + 1] = list[j];
+        j -= 1;
+      }
+      list[j + 1] = key;
+    }
+
+    return list;
+  }
+
 }
 
 module.exports = InsertionSort;
