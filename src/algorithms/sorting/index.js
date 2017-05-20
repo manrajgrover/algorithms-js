f3ac3f56e88476f4ddfa54343626aee2c8a5c64e
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-05-20 22:42:06 +0530

    Insertion Sort: Initialised

--- src/algorithms/sorting/index.js
+++ src/algorithms/sorting/index.js
@@ -1,7 +1,9 @@
 const BubbleSort = require('./BubbleSort');
+const InsertionSort = require('./InsertionSort');
 const SelectionSort = require('./SelectionSort');
 
 module.exports = {
   BubbleSort,
+  InsertionSort,
   SelectionSort
 };
