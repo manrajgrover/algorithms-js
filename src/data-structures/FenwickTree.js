09d4eb03e533b95d33db6c41207f7ee73c645f81
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-05-21 22:22:07 +0530

    Fenwick Tree: getSum method added

--- src/data-structures/FenwickTree.js
+++ src/data-structures/FenwickTree.js
@@ -4,6 +4,20 @@ class FenwickTree {
     this._list.fill(0);
     this._length = length;
   }
+
+  getSum(index = 0) {
+    let sum = 0;
+
+    index += 1;
+
+    while (index > 0) {
+      sum += this._list[index];
+
+      index -= (index & (-index));
+    }
+
+    return sum;
+  }
 }
 
 module.exports = FenwickTree;
