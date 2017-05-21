5e1530f0813e3ab4f6f44dba5c53e825b5d7068a
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-05-21 23:15:44 +0530

    Fenwick Tree: Changes constructor to use array as param

--- src/data-structures/FenwickTree.js
+++ src/data-structures/FenwickTree.js
@@ -1,8 +1,17 @@
 class FenwickTree {
-  constructor(length) {
-    this._list = new Array(length + 1);
+  constructor(array) {
+    this._list = array;
     this._list.fill(0);
-    this._length = length;
+    this._length = this._list.length;
+    this._buildTree(array);
+  }
+
+  get size() {
+    return this._length;
+  }
+
+  isEmpty() {
+    return this._length === 0;
   }
 
   getSum(index = 0) {
@@ -28,13 +37,12 @@ class FenwickTree {
     }
   }
 
-  buildTree(array) {
+  _buildTree(array) {
     if (!Array.isArray(array)) {
       throw new Error('Array needs to be passed in order to build the tree');
     }
 
-    const len = array.length;
-    for (let i = 0; i < len; i += 1) {
+    for (let i = 0; i < this._length; i += 1) {
       this.updateTree(i, array[i]);
     }
   }
