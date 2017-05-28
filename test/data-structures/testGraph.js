94b8d0ec50ed5f607c94b2207fab45ba0f16b3af
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-05-28 21:19:07 +0530

    Test Graph: Adds test to check for neighbours

--- test/data-structures/testGraph.js
+++ test/data-structures/testGraph.js
@@ -10,5 +10,21 @@ describe('Graph', () => {
     assert.equal(inst.size, 0);
     assert.deepStrictEqual(inst.vertices, []);
   });
+  it('should get neighbours', () => {
+    const inst = new Graph();
+    
+    inst.addVertex(1);
+    inst.addVertex(2);
+    inst.addVertex(3);
+    inst.addVertex(4);
+    inst.addVertex(5);
+
+    inst.addEdge(1, 2, 1);
+    inst.addEdge(1, 3, 1);
+    inst.addEdge(1, 5, 1);
+    inst.addEdge(3, 4, 1);
+    inst.addEdge(1, 5, 1);
+
+    assert.deepStrictEqual(inst.getNeighbours(1), ['2', '3', '5']);
   });
 });
