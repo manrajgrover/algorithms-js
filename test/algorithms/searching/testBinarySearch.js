bb7649346e95472416c695f0d93d4aaa02b52329
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-05-28 20:41:27 +0530

    Test Binary Search: 100% coverage

--- test/algorithms/searching/testBinarySearch.js
+++ test/algorithms/searching/testBinarySearch.js
@@ -8,4 +8,28 @@ describe('Binary Search', () => {
 
     assert.equal(index, -1);
   });
+
+  it('should return -1 for no element found in array', () => {
+    const index = binarysearch([2, 3, 4, 5, 6], 1);
+
+    assert.equal(index, -1);
+  });
+
+  it('should return index of mid element', () => {
+    const index = binarysearch([2, 3, 4, 5, 6], 4);
+
+    assert.equal(index, 2);
+  });
+
+  it('should return index of first element', () => {
+    const index = binarysearch([2, 3, 4, 5, 6], 2);
+
+    assert.equal(index, 0);
+  });
+
+  it('should return index of last element', () => {
+    const index = binarysearch([2, 3, 4, 5, 6], 6);
+
+    assert.equal(index, 4);
+  });
 });
