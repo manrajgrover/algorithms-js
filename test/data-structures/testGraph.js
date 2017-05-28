c6d8e23a4cbc9fe6d23f9bc0a024dbb58e83c390
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-05-28 21:19:52 +0530

    Test Graph: Adds test to check edge weight

--- test/data-structures/testGraph.js
+++ test/data-structures/testGraph.js
@@ -33,6 +33,29 @@ describe('Graph', () => {
     assert.deepStrictEqual(inst.vertices, ['1', '2', '3', '4', '5']);
   });
 
+  it('should get edge weight', () => {
+    const inst = new Graph();
+    
+    inst.addVertex(1);
+    inst.addVertex(2);
+    inst.addVertex(3);
+    inst.addVertex(4);
+    inst.addVertex(5);
+
+    inst.addEdge(1, 2, 1);
+    inst.addEdge(1, 3, 1);
+    inst.addEdge(1, 5, 1);
+    inst.addEdge(3, 4, 1);
+    inst.addEdge(1, 5, 1);
+    inst.addEdge(5, 6, 1);
+    inst.addEdge(7, 8, 1);
+
+    assert.equal(inst.getEdgeWeight(1, 2), 1);
+    assert.equal(inst.getEdgeWeight(2, 1), 1);
+
+    assert.throws(() => inst.getEdgeWeight(1, 4));
+  });
+
   it('should get neighbours', () => {
     const inst = new Graph();
     
