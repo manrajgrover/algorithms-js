92bd3f7365df54ea5487f5f412dad567e041e40c
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-05-28 21:18:35 +0530

    Test Graph: Initialisation test updated to check for vertices

--- test/data-structures/testGraph.js
+++ test/data-structures/testGraph.js
@@ -5,6 +5,10 @@ const assert = require('assert');
 describe('Graph', () => {
   it('should be empty when initialized', () => {
     const inst = new Graph();
+
     assert(inst.isEmpty());
+    assert.equal(inst.size, 0);
+    assert.deepStrictEqual(inst.vertices, []);
+  });
   });
 });
