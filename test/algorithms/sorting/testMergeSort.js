811d4f91c393bd2fe05d96139c3d8663ab46349e
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-06-02 23:49:05 +0530

    Test Merge Sort: Test array with equal vals

--- test/algorithms/sorting/testMergeSort.js
+++ test/algorithms/sorting/testMergeSort.js
@@ -18,4 +18,14 @@ describe('MergeSort', () => {
     assert.deepEqual(inst.sortedList, [1, 2, 3, 4]);
     assert.equal(inst.toString(), '1, 2, 3, 4');
   });
+
+  it('should sort the array in ascending order with few equal vals', () => {
+    const inst = new MergeSort([2, 1, 3, 4, 2], (a, b) => a < b);
+    assert.equal(inst.size, 5);
+
+    assert.deepEqual(inst.unsortedList, [2, 1, 3, 4, 2]);
+    assert.deepEqual(inst.sortedList, [1, 2, 2, 3, 4]);
+    assert.equal(inst.toString(), '1, 2, 2, 3, 4');
+  });
+
 });
