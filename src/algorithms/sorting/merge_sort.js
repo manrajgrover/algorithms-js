a6abc5bf603fd70592db30a1112cffccf045bfd0
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-06-02 23:42:37 +0530

    Merge Sort: Get size of array

--- src/algorithms/sorting/merge_sort.js
+++ src/algorithms/sorting/merge_sort.js
@@ -6,6 +6,15 @@ class MergeSort {
     this._sortedList = this._sort(data.slice());
     this._length = data.length;
   }
+
+  /**
+   * Get size of array
+   * @return {Number} Size of array
+   */
+  get size() {
+    return this._length;
+  }
+
 }
 
 module.exports = MergeSort;
