54989be25a147b6b87192a15a010f77e46a08c56
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-06-02 23:48:34 +0530

    Test Merge Sort: Sorting array

--- test/algorithms/sorting/testMergeSort.js
+++ test/algorithms/sorting/testMergeSort.js
@@ -9,4 +9,13 @@ describe('MergeSort', () => {
     assert.deepEqual(inst.unsortedList, []);
     assert.deepEqual(inst.sortedList, []);
   });
+
+  it('should sort the array', () => {
+    const inst = new MergeSort([2, 1, 3, 4]);
+    assert.equal(inst.size, 4);
+
+    assert.deepEqual(inst.unsortedList, [2, 1, 3, 4]);
+    assert.deepEqual(inst.sortedList, [1, 2, 3, 4]);
+    assert.equal(inst.toString(), '1, 2, 3, 4');
+  });
 });
