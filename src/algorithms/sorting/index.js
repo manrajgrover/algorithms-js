2e6fbced4742e19baf87e043f9300147f6e2e106
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-06-02 23:40:51 +0530

    Index: Export Merge Sort

--- src/algorithms/sorting/index.js
+++ src/algorithms/sorting/index.js
@@ -1,9 +1,11 @@
 const BubbleSort = require('./bubble_sort');
 const InsertionSort = require('./insertion_sort');
+const MergeSort = require('./merge_sort');
 const SelectionSort = require('./selection_sort');
 
 module.exports = {
   BubbleSort,
   InsertionSort,
+  MergeSort,
   SelectionSort
 };
