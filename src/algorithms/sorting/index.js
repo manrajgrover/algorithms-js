c8a56ec12c3af7259422e4c55219a3309c947351
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-06-03 22:46:19 +0530

    Heap Sort: Exported module

--- src/algorithms/sorting/index.js
+++ src/algorithms/sorting/index.js
@@ -1,10 +1,12 @@
 const BubbleSort = require('./bubble_sort');
+const HeapSort = require('./heap_sort');
 const InsertionSort = require('./insertion_sort');
 const MergeSort = require('./merge_sort');
 const SelectionSort = require('./selection_sort');
 
 module.exports = {
   BubbleSort,
+  HeapSort,
   InsertionSort,
   MergeSort,
   SelectionSort
