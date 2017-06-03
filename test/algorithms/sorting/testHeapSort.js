27982b6392b8dcd2adc22fb0db7eb0aecab0f62a
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-06-03 23:12:35 +0530

    Test Heap Sort: Sort array with equal vals

--- test/algorithms/sorting/testHeapSort.js
+++ test/algorithms/sorting/testHeapSort.js
@@ -19,4 +19,12 @@ describe('HeapSort', () => {
     assert.equal(inst.toString(), '1, 2, 3, 4');
   });
 
+  it('should sort the array in ascending order with few equal vals', () => {
+    const inst = new HeapSort([2, 1, 3, 4, 2], (a, b) => a < b);
+    assert.equal(inst.size, 5);
+
+    assert.deepEqual(inst.unsortedList, [2, 1, 3, 4, 2]);
+    assert.deepEqual(inst.sortedList, [1, 2, 2, 3, 4]);
+    assert.equal(inst.toString(), '1, 2, 2, 3, 4');
+  });
 });
