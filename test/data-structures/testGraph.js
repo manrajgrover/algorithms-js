765ab3f23fb128fa823345aabcd531d1428bba07
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-06-05 23:36:46 +0530

    Test Graph: Directed graphs test added

--- test/data-structures/testGraph.js
+++ test/data-structures/testGraph.js
@@ -3,7 +3,7 @@ const Graph = require('../../src').datastructures.Graph;
 const assert = require('assert');
 
 describe('Graph', () => {
-  it('should be empty when initialized', () => {
+  it('should be empty when initializing undirected graph', () => {
     const inst = new Graph();
 
     assert(inst.isEmpty());
@@ -72,5 +72,31 @@ describe('Graph', () => {
     inst.addEdge(1, 5, 1);
 
     assert.deepStrictEqual(inst.getNeighbours(1), ['2', '3', '5']);
+  });
+
+  it('should be empty when initializing directed graph', () => {
+    const inst = new Graph(true);
+
+    assert(inst.isEmpty());
+    assert.equal(inst.size, 0);
+    assert.deepStrictEqual(inst.vertices, []);
+  });
+
+  it('should get neighbours from directed graph', () => {
+    const inst = new Graph(true);
+
+    inst.addVertex(1);
+    inst.addVertex(2);
+    inst.addVertex(3);
+    inst.addVertex(4);
+    inst.addVertex(5);
+
+    inst.addEdge(1, 2, 1);
+    inst.addEdge(1, 3, 1);
+    inst.addEdge(1, 5, 1);
+    inst.addEdge(3, 4, 1);
+
+    assert.deepStrictEqual(inst.getNeighbours(1), ['2', '3', '5']);
+    assert.deepStrictEqual(inst.getNeighbours(3), ['4']);
   });
 });
