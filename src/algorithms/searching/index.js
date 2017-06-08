a7f6163c193b4681cdc38ffe92cbb60d804f8eb6
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-06-08 23:13:28 +0530

    Interpolation Search: Export as module

--- src/algorithms/searching/index.js
+++ src/algorithms/searching/index.js
@@ -1,7 +1,9 @@
 const binarysearch = require('./binary_search');
+const interpolationsearch = require('./interpolation_search');
 const linearsearch = require('./linear_search');
 
 module.exports = {
   binarysearch,
+  interpolationsearch,
   linearsearch
 };
