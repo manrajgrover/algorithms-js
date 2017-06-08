0ef0a54411953197e9ed0e5aea249f5646308362
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-06-08 23:13:56 +0530

    Interpolation Search: Initialised

---
+++ src/algorithms/searching/interpolation_search.js
@@ -0,0 +1,32 @@
+/**
+ * Interpolation Search Algorithm
+ * @param  {Array}  sortedArray Sorted Array to be searched
+ * @param  {Number} element     Element to be searched
+ * @return {Number}             Index of the element, if found
+ */
+const interpolationsearch = (sortedArray, element) => {
+  let left = 0;
+  let right = sortedArray.length - 1;
+
+  while ((left <= right) && (element >= sortedArray[left]) && (element <= sortedArray[right])) {
+    const valDiff = sortedArray[right] - sortedArray[left];
+    const posDiff = right - left;
+    const elementDiff = element - sortedArray[left];
+
+    const pos = left + ((posDiff * elementDiff) / valDiff);
+
+    if (sortedArray[pos] === element) {
+      return pos;
+    }
+
+    if (sortedArray[pos] < element) {
+      left = pos + 1;
+    } else {
+      right = pos - 1;
+    }
+  }
+
+  return -1;
+};
+
+module.exports = interpolationsearch;
