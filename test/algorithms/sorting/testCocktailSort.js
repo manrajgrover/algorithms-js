d5c82f8c43ef70d1ac860fafa86af110d79238ab
Ujjwal Upadhyay <Ujjwal Upadhyay>
2017-06-09 06:07:50 +0530

    corrected an error in Test case for cocktail sort

--- test/algorithms/sorting/testCocktailSort.js
+++ test/algorithms/sorting/testCocktailSort.js
@@ -1,5 +1,5 @@
 var cocktailsort = require('../src/algorithms/sorting/cocktail_sort.js');
 const assert = require('assert');
 var array = [5,100,203,-29,9,-53];
-cocktailsort(array);
+inst = cocktailsort(array);
 assert.equal(inst.toString(), '-53, -29, 5, 9, 100, 203');
