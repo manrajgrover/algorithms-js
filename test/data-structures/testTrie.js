084584812e0a1893328bb2c1f1dce97a4a33b258
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-06-26 21:29:30 +0530

    Test Trie: Return value -1 if not exist

--- test/data-structures/testTrie.js
+++ test/data-structures/testTrie.js
@@ -25,59 +25,59 @@ describe('Trie', () => {
   it('should search words in Trie', () => {
     const inst = new Trie();
 
-    inst.insert('cool');
-    inst.insert('the');
-    inst.insert('their');
-    inst.insert('them');
+    inst.insert('cool', 1);
+    inst.insert('the', 2);
+    inst.insert('their', 3);
+    inst.insert('them', 4);
 
-    assert(inst.search('the'));
-    assert(inst.search('them'));
-    assert(!inst.search('they'));
+    assert.equal(inst.search('the'), 2);
+    assert.equal(inst.search('them'), 4);
+    assert.equal(inst.search('they'), -1);
 
-    assert(!inst.search('coo'));
-    assert(inst.search('cool'));
+    assert.equal(inst.search('coo'), -1);
+    assert.equal(inst.search('cool'), 1);
   });
 
   it('should search with deleted words in Trie', () => {
     const inst = new Trie();
 
-    inst.insert('cool');
-    inst.insert('the');
-    inst.insert('their');
-    inst.insert('them');
-    inst.insert('themselves');
+    inst.insert('cool', 1);
+    inst.insert('the', 2);
+    inst.insert('their', 3);
+    inst.insert('them', 4);
+    inst.insert('themselves', 5);
 
     inst.delete('them');
-    assert(!inst.search('them'));
+    assert.equal(inst.search('them'), -1);
 
     assert.equal(inst.size, 4);
 
     inst.delete('the');
     assert.equal(inst.size, 3);
-    assert(!inst.search('the'));
-    assert(inst.search('themselves'));
+    assert.equal(inst.search('the'), -1);
+    assert.equal(inst.search('themselves'), 5);
 
     inst.delete('themselves');
     assert.equal(inst.size, 2);
-    assert(!inst.search('themselves'));
+    assert.equal(inst.search('themselves'), -1);
   });
 
   it('should try deleting deleted word', () => {
     const inst = new Trie();
 
-    inst.insert('cool');
-    inst.insert('the');
-    inst.insert('their');
-    inst.insert('them');
-    inst.insert('themselves');
+    inst.insert('cool', 1);
+    inst.insert('the', 2);
+    inst.insert('their', 3);
+    inst.insert('them', 4);
+    inst.insert('themselves', 5);
 
     inst.delete('them');
-    assert(!inst.search('them'));
+    assert.equal(inst.search('them'), -1);
 
     assert.equal(inst.size, 4);
 
     assert(!inst.delete('them'));
     assert.equal(inst.size, 4);
-    assert(!inst.search('them'));
+    assert.equal(inst.search('them'), -1);
   });
 });
