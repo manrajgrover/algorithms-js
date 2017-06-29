de3271cfe57ad4abe75dce662cf86a03763aded0
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-06-29 23:12:32 +0530

    Jump Search: Initialised

--- src/algorithms/searching/index.js
+++ src/algorithms/searching/index.js
@@ -1,9 +1,11 @@
 const binarysearch = require('./binary_search');
 const interpolationsearch = require('./interpolation_search');
+const jumpsearch = require('./jump_search');
 const linearsearch = require('./linear_search');
 
 module.exports = {
   binarysearch,
   interpolationsearch,
+  jumpsearch,
   linearsearch
 };
