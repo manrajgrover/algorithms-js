6b486c75ba25a1ef10ff9a37f6e8bea6bdea1720
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-08-04 23:14:22 +0530

    Test Ternary Search: Adds tests

---
+++ test/algorithms/search/testTernarySearch.js
@@ -0,0 +1,35 @@
+/* eslint-env mocha */
+const ternarysearch = require('../../../src').algorithms.search.ternarysearch;
+const assert = require('assert');
+
+describe('Ternary Search', () => {
+  it('should return -1 for empty array', () => {
+    const index = ternarysearch([], 1);
+
+    assert.equal(index, -1);
+  });
+
+  it('should return -1 for no element found in array', () => {
+    const index = ternarysearch([2, 3, 4, 5, 6], 1);
+
+    assert.equal(index, -1);
+  });
+
+  it('should return index of mid element', () => {
+    const index = ternarysearch([2, 3, 4, 5, 6], 4);
+
+    assert.equal(index, 2);
+  });
+
+  it('should return index of first element', () => {
+    const index = ternarysearch([2, 3, 4, 5, 6], 2);
+
+    assert.equal(index, 0);
+  });
+
+  it('should return index of last element', () => {
+    const index = ternarysearch([2, 3, 4, 5, 6], 6);
+
+    assert.equal(index, 4);
+  });
+});
