66113e2495a245dc92680389f5d65c9ab8370373
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-08-04 23:07:41 +0530

    Search: Ternary Search exported

--- src/algorithms/search/index.js
+++ src/algorithms/search/index.js
@@ -2,10 +2,12 @@ const binarysearch = require('./binary_search');
 const interpolationsearch = require('./interpolation_search');
 const jumpsearch = require('./jump_search');
 const linearsearch = require('./linear_search');
+const ternarysearch = require('./ternary_search');
 
 module.exports = {
   binarysearch,
   interpolationsearch,
   jumpsearch,
-  linearsearch
+  linearsearch,
+  ternarysearch
 };
