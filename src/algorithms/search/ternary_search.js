cf2868862d707c61903a4c8e5e63386a8a53f078
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-08-04 23:06:20 +0530

    Search: Ternary Search implemented

---
+++ src/algorithms/search/ternary_search.js
@@ -0,0 +1,32 @@
+/**
+ * Ternary Search Algorithm
+ * @param  {Array}  sortedArray Sorted Array to be searched
+ * @param  {Number} element     Element to be searched
+ * @return {Number}             Index of the element, if found
+ */
+const ternarysearch = (sortedArray, element) => {
+  let left = 0;
+  let right = sortedArray.length - 1;
+
+  while (left <= right) {
+    const firstMid = (left) + ((right - left) / 3);
+    const secondMid = (firstMid) + ((right - left) / 3);
+
+    if (sortedArray[firstMid] === element) {
+      return firstMid;
+    } else if (sortedArray[secondMid] === element) {
+      return secondMid;
+    } else if (sortedArray[firstMid] > element) {
+      right = firstMid - 1;
+    } else if (sortedArray[secondMid] < element) {
+      left = secondMid - 1;
+    } else {
+      left = firstMid + 1;
+      right = secondMid - 1;
+    }
+  }
+
+  return -1;
+};
+
+module.exports = ternarysearch;
