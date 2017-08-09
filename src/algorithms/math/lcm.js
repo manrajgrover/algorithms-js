ff4c6d3b49329193551c6c21415e69f23968dc62
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-08-09 23:16:49 +0530

    LCM: Bug fixes for negative and 0 inputs

--- src/algorithms/math/lcm.js
+++ src/algorithms/math/lcm.js
@@ -1,5 +1,18 @@
 const gcd = require('./gcd');
 
-const lcm = (a, b) => (a * b) / gcd(a, b);
+/**
+ * Calculates LCM of two numbers
+ * @param  {Number} a First number
+ * @param  {Number} b Second number
+ * @return {Number}   LCM of two numbers
+ */
+const lcm = (a, b) => {
+  if (a === 0 || b === 0) {
+    return 0;
+  }
+  a = Math.abs(a);
+  b = Math.abs(b);
+  return (a * b) / gcd(a, b);
+};
 
 module.exports = lcm;
