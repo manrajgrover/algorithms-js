1853589612e5e81dba9416a54000d5ea8be850c2
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-08-10 23:25:07 +0530

    Fast Exp: Initialized

---
+++ src/algorithms/math/fast_exp.js
@@ -0,0 +1,23 @@
+/**
+ * Raises base to a power keeping mod in check
+ * @param  {Number} a   Base
+ * @param  {Number} e   Power
+ * @param  {Number} mod Mod Value
+ * @return {Number}     Base to a power keeping mod in check
+ */
+const fastexp = (a, e, mod = 1e9 + 7) => {
+  let res = 1;
+
+  while (e > 0) {
+    if (e % 2 === 1) {
+      res = (res * a) % mod;
+    }
+
+    a = (a * a) % mod;
+    e /= 2;
+  }
+
+  return res % mod;
+};
+
+module.exports = fastexp;
