fad55fe500ef5c60bc8eebbeae16d8f9aa1acb89
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-08-22 23:46:23 +0530

    Extended Euclidean: Exported

--- src/algorithms/math/index.js
+++ src/algorithms/math/index.js
@@ -1,8 +1,10 @@
+const extendedEuclidean = require('./extended_euclidean');
 const gcd = require('./gcd');
 const fastexp = require('./fast_exp');
 const lcm = require('./lcm');
 
 module.exports = {
+  extendedEuclidean,
   gcd,
   fastexp,
   lcm
