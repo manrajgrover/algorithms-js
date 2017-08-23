aabfd5e536606950436745e47fb6e057802ac0f1
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-08-23 23:33:43 +0530

    Test Extended Euclidean: Add more test cases

--- test/algorithms/math/testExtendedEuclidean.js
+++ test/algorithms/math/testExtendedEuclidean.js
@@ -4,7 +4,21 @@ const extendedEuclidean = require('../../../src').algorithms.math.extendedEuclid
 const assert = require('assert');
 
 describe('Extended Euclidean', () => {
-  it('should check for both numbers as 0', () => {
+  it('should check for numbers as 0', () => {
+    assert.deepStrictEqual(extendedEuclidean(35, 0), { gcd: 35, x: 1, y: 0 });
+    assert.deepStrictEqual(extendedEuclidean(0, 15), { gcd: 15, x: 0, y: 1 });
+    assert.deepStrictEqual(extendedEuclidean(0, 0), { gcd: 0, x: 0, y: 1 });
+  });
+
+  it('should check extended euclidean values for positive values of a and b', () => {
     assert.deepStrictEqual(extendedEuclidean(35, 15), { gcd: 5, x: 1, y: -2 });
+    assert.deepStrictEqual(extendedEuclidean(32, 1), { gcd: 1, x: 0, y: 1 });
+    assert.deepStrictEqual(extendedEuclidean(1, 32), { gcd: 1, x: 1, y: 0 });
+  });
+
+  it('should check extended euclidean values for negative values of a and b', () => {
+    assert.deepStrictEqual(extendedEuclidean(35, -15), { gcd: 5, x: 1, y: 2 });
+    assert.deepStrictEqual(extendedEuclidean(-35, 15), { gcd: -5, x: 1, y: 2 });
+    assert.deepStrictEqual(extendedEuclidean(-35, -15), { gcd: -5, x: 1, y: -2 });
   });
 });
