b3216e062662952b1a42d551e2e1e9fd58fb97bf
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-08-30 23:13:32 +0530

    Geometry: Exported

--- src/algorithms/index.js
+++ src/algorithms/index.js
@@ -1,8 +1,10 @@
+const geometry = require('./geometry');
 const math = require('./math');
 const search = require('./search');
 const sort = require('./sort');
 
 module.exports = {
+  geometry,
   math,
   search,
   sort
