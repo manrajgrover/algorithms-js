322269ce3be4ab272db6a7a9945a3eb556881412
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-09-12 23:12:02 +0530

    String: Exported

--- src/algorithms/index.js
+++ src/algorithms/index.js
@@ -1,11 +1,13 @@
 const geometry = require('./geometry');
 const math = require('./math');
+const string = require('./string');
 const search = require('./search');
 const sort = require('./sort');
 
 module.exports = {
   geometry,
   math,
+  string,
   search,
   sort
 };
