324979d229efc631ba520c2c3e7c45adaa30008f
Enric Florit <efz1005@gmail.com>
2017-10-25 20:30:14 +0200

    fix style

--- src/algorithms/math/index.js
+++ src/algorithms/math/index.js
@@ -2,6 +2,9 @@ const extendedEuclidean = require('./extended_euclidean');
 const gcd = require('./gcd');
 const fastexp = require('./fast_exp');
 const lcm = require('./lcm');
+const fibonacci = require('./fibonacci');
+const divisors = require('./divisors');
+const isprime = require('./isprime');
 
 module.exports = {
   extendedEuclidean,
