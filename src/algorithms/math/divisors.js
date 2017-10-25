8e7d08e35dd7d5a43550ec64721c7c6a5218b8fe
Enric Florit <efz1005@gmail.com>
2017-10-25 20:38:16 +0200

    const

--- src/algorithms/math/divisors.js
+++ src/algorithms/math/divisors.js
@@ -1,10 +1,11 @@
 const divisors = (n = 1) => {
-  let divi = [1];
+  const divi = [1];
   let d = 2;
   while (d <= n) {
     if (n % d === 0) {
       divi.push(d);
     }
+    d += 1;
   }
 
   return divi;
--- src/algorithms/math/fibonacci.js
+++ src/algorithms/math/fibonacci.js
@@ -1,5 +1,5 @@
 const fibonacci = (n = 1) => {
-  let mem = [0, 1];
+  const mem = [0, 1];
   let i = 2;
   while (i <= n) {
     mem[i] = mem[i - 1] + mem[i - 2];
