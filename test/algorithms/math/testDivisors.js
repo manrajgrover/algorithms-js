6771d95e08065e7b1f1479517a2ac356c25af2d8
Enric Florit <efz1005@gmail.com>
2017-10-25 20:44:47 +0200

    deepequal

--- test/algorithms/math/testDivisors.js
+++ test/algorithms/math/testDivisors.js
@@ -5,13 +5,13 @@ const assert = require('assert');
 
 describe('Divisors', () => {
   it('should work for 1 and primes', () => {
-    assert.equal(divisors(1), [1]);
-    assert.equal(divisors(2), [1, 2]);
-    assert.equal(divisors(3), [1, 3]);
-    assert.equal(divisors(97), [1, 97]);
+    assert.deepEqual(divisors(1), [1]);
+    assert.deepEqual(divisors(2), [1, 2]);
+    assert.deepEqual(divisors(3), [1, 3]);
+    assert.deepEqual(divisors(97), [1, 97]);
   });
 
   it('should work for big numbers', () => {
-    assert.equal(divisors(60), [1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30, 60]);
+    assert.deepEqual(divisors(60), [1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30, 60]);
   });
 });
