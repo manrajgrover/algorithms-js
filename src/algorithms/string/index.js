17e5220e98d094a174f9f116e074f3f6203e61d5
Enric Florit <efz1005@gmail.com>
2017-10-25 19:52:54 +0200

    __

--- src/algorithms/string/index.js
+++ src/algorithms/string/index.js
@@ -1,6 +1,6 @@
 const levenshteindistance = require('./levenshtein_distance');
-const longestcommonleadingsubstring = require('./longestcommonleadingsubstring');
-const longestcommontrailingsubstring = require('./longestcommontrailingsubstring');
+const longestcommonleadingsubstring = require('./longest_common_leading_substring');
+const longestcommontrailingsubstring = require('./longest_common_trailing_substring');
 
 module.exports = {
   levenshteindistance,
