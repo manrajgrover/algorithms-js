a13dda22ffe010d6555fcc07e63638c0f2f2f206
Enric Florit <efz1005@gmail.com>
2017-10-25 19:56:47 +0200

    Style fixes

--- src/algorithms/string/longest_common_leading_substring.js
+++ src/algorithms/string/longest_common_leading_substring.js
@@ -1,12 +1,12 @@
 /* source: https://stackoverflow.com/questions/1916218/find-the-longest-common-starting-substring-in-a-set-of-strings#1917041 */
 const longestcommonleadingsubstring = (array) => {
-    var A = array.concat().sort(), 
-    	a1 = A[0], 
-    	a2 = A[A.length-1], 
-    	L = a1.length, 
-    	i = 0;
-    while (i < L && a1.charAt(i) === a2.charAt(i)) i++;
-    return a1.substring(0, i);
+  var A = array.concat().sort()
+  var a1 = A[0];
+  var a2 = A[A.length-1];
+  var L = a1.length;
+  var i = 0;
+  while (i < L && a1.charAt(i) === a2.charAt(i)) i += 1;
+  return a1.substring(0, i);
 };
 
 module.exports = longestcommonleadingsubstring;
--- src/algorithms/string/longest_common_trailing_substring.js
+++ src/algorithms/string/longest_common_trailing_substring.js
@@ -1,11 +1,11 @@
 const longestcommontrailingsubstring = (array) => {
-    var A = array.concat().sort(), 
-    	a1 = A[0], 
-    	a2 = A[A.length-1], 
-    	L = a1.length, 
-    	i = L - 1;
-    while (i >= 0 && a1.charAt(i) === a2.charAt(i)) i--;
-    return a1.substring(i - 1, L);
+  var A = array.concat().sort();
+  var a1 = A[0];
+  var a2 = A[A.length-1];
+  var L = a1.length;
+  var i = L - 1;
+  while (i >= 0 && a1.charAt(i) === a2.charAt(i)) i -= 1;
+  return a1.substring(i - 1, L);
 };
 
 module.exports = longestcommontrailingsubstring;
