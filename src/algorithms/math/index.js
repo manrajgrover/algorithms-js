993f00b07d3b324516c9f0a889fa846f22599cfd
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-11-06 23:30:24 +0530

    Modular Inverse: exported

--- src/algorithms/math/index.js
+++ src/algorithms/math/index.js
@@ -2,10 +2,12 @@ const extendedEuclidean = require('./extended_euclidean');
 const gcd = require('./gcd');
 const fastexp = require('./fast_exp');
 const lcm = require('./lcm');
+const modularInverse = require('./modular_inverse');
 
 module.exports = {
   extendedEuclidean,
   gcd,
   fastexp,
-  lcm
+  lcm,
+  modularInverse
 };
