7a9b6f1cce8978d57f71088a22b85d0cc81c9690
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-11-07 22:25:24 +0530

    Tests Modular Inverse: Initialized

---
+++ test/algorithms/math/testModularInverse.js
@@ -0,0 +1,6 @@
+/* eslint-env mocha */
+const modularInverse = require('../../../src').algorithms.math.modularInverse;
+
+const assert = require('assert');
+
+describe('Modular Inverse', () => { });
