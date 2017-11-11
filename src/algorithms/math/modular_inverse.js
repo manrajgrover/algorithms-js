38fbcca014a46adf58f80089911e62ed63f2bf1d
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-11-11 11:12:50 +0530

    Modular Inverse: Fix modular bugs

--- src/algorithms/math/modular_inverse.js
+++ src/algorithms/math/modular_inverse.js
@@ -1,25 +1,6 @@
 const exEuclidean = require('./extended_euclidean');
 const fastExp = require('./fast_exp');
 
-/**
- * Calculates modular inverse of a number
- *
- * @param  {Number} a Number for which inverse needs to be found
- * @param  {Number} m Mod val
- * @return {Number}   Modular Inverse
- */
-const modularInverse = (a, m) => {
-  const result = exEuclidean(a, m);
-  if (result.gcd !== 1) {
-    return null;
-  }
-
-  console.log(result);
-
-  return (result.x + m) % m;
-};
-
-module.exports = modularInverse;
 
 /**
  * Calculates modular inverse of a number using Fermet's theorem
@@ -28,6 +9,36 @@ module.exports = modularInverse;
  * @param  {Number} m Mod val
  * @return {Number}   Modular Inverse
  */
-const fermetModularInverse = (a, m) => fastExp(a, m - 2, m);
+const fermetModularInverse = (a, m) => {
+  if (a === 0 || m === 0) {
+    return null;
+  }
 
-module.exports = fermetModularInverse;
+  return fastExp(a, m - 2, m);
+};
+
+/**
+ * Calculates modular inverse of a number
+ *
+ * @param  {Number} a Number for which inverse needs to be found
+ * @param  {Number} m Mod val
+ * @return {Number}   Modular Inverse
+ */
+const modularInverse = (a, m) => {
+  if (a === 0 || m === 0) {
+    return null;
+  }
+
+  const result = exEuclidean(a, m);
+
+  if (result.gcd !== 1) {
+    return null;
+  }
+
+  return (result.x + m) % m;
+};
+
+module.exports = {
+  fermetModularInverse,
+  modularInverse
+};
