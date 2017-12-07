e2e53ab54a4310e9d7355603389caabb80dba4e2
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-12-08 00:45:18 +0530

    Levenshtein Distance: Array size bugs fixed

--- src/algorithms/string/levenshtein_distance.js
+++ src/algorithms/string/levenshtein_distance.js
@@ -7,15 +7,19 @@ const levenshteindistance = (a = '', b = '') => {
     return a.length;
   }
 
-  const dp = [...Array(a.length)].map(() => Array(b.length));
+  const dp = [...Array(b.length + 1)].map(() => Array(a.length + 1));
 
   for (let i = 0; i <= b.length; i += 1) {
-    for (let j = 0; j <= a.length; j += 1) {
-      if (i === 0) {
-        dp[i][j] = j;
-      } else if (j === 0) {
-        dp[i][j] = i;
-      } else if (b.charAt(i - 1) === a.charAt(j - 1)) {
+    dp[i][0] = i;
+  }
+
+  for (let i = 0; i <= a.length; i += 1) {
+    dp[0][i] = i;
+  }
+
+  for (let i = 1; i <= b.length; i += 1) {
+    for (let j = 1; j <= a.length; j += 1) {
+      if (b.charAt(i - 1) === a.charAt(j - 1)) {
         dp[i][j] = dp[i - 1][j - 1];
       } else {
         dp[i][j] = Math.min(dp[i - 1][j - 1] + 1, Math.min(dp[i][j - 1] + 1, dp[i - 1][j] + 1));
