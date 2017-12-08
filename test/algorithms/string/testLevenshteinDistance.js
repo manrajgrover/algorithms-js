ffc93ab799bde854cc4d7df53c7a3d29cbb35f05
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-12-08 23:28:53 +0530

    Test Levenshtein Distance: Tests for edge cases

--- test/algorithms/string/testLevenshteinDistance.js
+++ test/algorithms/string/testLevenshteinDistance.js
@@ -3,11 +3,32 @@ const levenshteindistance = require('../../../src').algorithms.string.levenshtei
 const assert = require('assert');
 
 describe('Levenshtein Distance', () => {
-  it('should have no data when empty initialization', () => {
+  it('should find levenshtein distance between two strings', () => {
     const stringA = 'sunday';
     const stringB = 'saturday';
 
     const distance = levenshteindistance(stringA, stringB);
     assert.equal(distance, 3);
+  });
+
+  it('should return length of string A when B is empty', () => {
+    const stringA = 'sunday';
+    const stringB = '';
+
+    const distance = levenshteindistance(stringA, stringB);
+    assert.equal(distance, 6);
+  });
+
+  it('should return length of string B when A is empty', () => {
+    const stringA = '';
+    const stringB = 'saturday';
+
+    const distance = levenshteindistance(stringA, stringB);
+    assert.equal(distance, 8);
+  });
+
+  it('should return distance as 0 when no params are provided', () => {
+    const distance = levenshteindistance();
+    assert.equal(distance, 0);
   });
 });
