8609a563da44bb14a146bc8ff32ac94554e56da0
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-12-09 22:09:49 +0530

    Test Depth First Search: Initialized

---
+++ test/algorithms/search/testDepthFirstSearch.js
@@ -0,0 +1,51 @@
+/* eslint-env mocha */
+const dfs = require('../../../src').algorithms.search.dfs;
+const assert = require('assert');
+
+describe('DFS', () => {
+  it('should perform dfs on a tree', () => {
+    const tree = {
+      root: {
+        name: 'root',
+        children: [{
+          name: 'child_1',
+          children: [{
+            name: 'child_4',
+            children: []
+          }, {
+            name: 'child_5',
+            children: []
+          }]
+        }, {
+          name: 'child_2',
+          children: [{
+            name: 'child_6',
+            children: []
+          }]
+        }, {
+          name: 'child_3',
+          children: [{
+            name: 'child_7',
+            children: []
+          }]
+        }]
+      }
+    };
+
+    const children = [];
+    dfs(tree.root, 'children', node => children.push(node.name));
+    console.log(children);
+    assert.deepStrictEqual(
+      children, [
+        'root',
+        'child_3',
+        'child_7',
+        'child_2',
+        'child_6',
+        'child_1',
+        'child_5',
+        'child_4'
+      ]
+    );
+  });
+});
