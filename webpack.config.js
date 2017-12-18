1770a95b98f65be3960be26c9ed6fe4092a6b0f0
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-12-18 23:40:23 +0530

    Webpack: Add library target to run it in browser

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,21 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: {
-    algorithms: './src/index.js',
-    'algorithms.min': './src/index.js'
+  entry: {algorithms: './src/index.js', 'algorithms.min': './src/index.js'},
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    library: 'alds'
   },
-  output: { path: path.resolve(__dirname, 'dist'), filename: '[name].js' },
   module: {
     rules: [{
       test: [/\.es6$/, /\.js$/],
       exclude: /(node_modules|bower_components)/,
-      use: { loader: 'babel-loader', options: { presets: 'es2015' } }
+      use: {loader: 'babel-loader', options: {presets: 'es2015'}}
     }]
   },
   plugins: [new webpack.optimize.UglifyJsPlugin(
-      { include: /\.min\.js$/, minimize: true })],
+      {include: /\.min\.js$/, minimize: true})],
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
     compress: true,
