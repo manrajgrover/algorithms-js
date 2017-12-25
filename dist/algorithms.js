11258b0cd592ac2a498c26fd268757a7cd9a705f
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-12-25 22:27:26 +0530

    Dist: Library should now work in all environments

--- dist/algorithms.js
+++ dist/algorithms.js
@@ -1,5 +1,14 @@
-var alds =
-/******/ (function(modules) { // webpackBootstrap
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["alds"] = factory();
+	else
+		root["alds"] = factory();
+})(typeof self !== 'undefined' ? self : this, function() {
+return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
@@ -4817,3 +4826,4 @@ module.exports = SelectionSort;
 
 /***/ })
 /******/ ]);
+});
