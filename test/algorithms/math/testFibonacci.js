414a95466aab539ece8638c33f5755a864c9dfac
Htoo Pyae <34838719+hpbyte@users.noreply.github.com>
2018-10-14 21:52:46 +0630

    Updated fibonacci test

--- test/algorithms/math/testFibonacci.js
+++ test/algorithms/math/testFibonacci.js
@@ -3,19 +3,19 @@ const fibonacci = require('../../../src/algorithms/math/fibonacci');
 const assert = require('assert');
 
 describe('Fibonacci', () => {
-    it('it should return 0 if given number is 0', () => {
-        assert.deepStrictEqual(fibonacci(0), '0');
+    it('should return 0 if given number is 0', () => {
+        assert.deepStrictEqual(fibonacci(0), [0]);
     });
 
-    it('should return message if given number is negative', () => {
-        assert.deepStrictEqual(fibonacci(-5), 'Plz provide positive number');
+    it('should throw an error if the number is negative', () => {
+        assert.throws(() => fibonacci(-5))
     });
 
     it('should return fibonacci sequence up to 10', () => {
-        assert.deepStrictEqual(fibonacci(10), '0 1 1 2 3 5 8');
+        assert.deepStrictEqual(fibonacci(10), [0,1,1,2,3,5,8]);
     });
 
     it('should return fibonacci sequence up to 100', () => {
-        assert.deepStrictEqual(fibonacci(100), '0 1 1 2 3 5 8 13 21 34 55 89');
+        assert.deepStrictEqual(fibonacci(100), [0,1,1,2,3,5,8,13,21,34,55,89]);
     });
-})
+});
