ac610b64c04bdcce4e6f4bebf96c99802d604d17
Htoo Pyae <34838719+hpbyte@users.noreply.github.com>
2018-10-14 21:52:00 +0630

    Fibonacci Updated

--- src/algorithms/math/fibonacci.js
+++ src/algorithms/math/fibonacci.js
@@ -4,21 +4,21 @@
  */
 const fibonacci = (num) => {
     let n1 = 0, n2 = 1;
-    let str = '';
+    let fibo = [];
 
     if (num < 0) {
-        return 'Plz provide positive number';
+        throw new Error("Not a positive number");
     }
 
     while (n1 <= num) {
-        str += `${n1} `;
+        fibo.push(n1);
 
         let sum = n1 + n2;
         n1 = n2;
         n2 = sum;
     }
 
-    return str.trimEnd();
+    return fibo;
 };
 
 module.exports = fibonacci;
