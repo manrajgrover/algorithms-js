8a2af3db67e5aa040e441da98844b6f3afadb4b4
MrfksIv <cmorphakis@gmail.com>
2018-10-20 15:25:42 +0300

    newton square root method added with appended docs, tests and linter errors fixed

--- src/algorithms/math/newtons_square_root.js
+++ src/algorithms/math/newtons_square_root.js
@@ -1,38 +1,38 @@
-/** 
- * Calculates the square root of a number using Newton's 
+/**
+ * Calculates the square root of a number using Newton's
  * recursive method.
- * 
+ *
  * @param {Number}  a           Number for which the square root needs to be found
  * @param {Integer} maxIter     The maximum number of iterations that the algorithm will repeat
  *                              unless the required accuracy is reached first.
- * @param {Number}  accuracy    The algorithm will run the necessary number of times 
- *                              the required accuracy is reached or until the max_iter is reached first.
+ * @param {Number}  accuracy    The algorithm will run the necessary number of times
+ *                              the required accuracy is reached or until the max_iter is
+ *                              reached first.
  * @return {Number} The square root of a
  */
 
-const newtonSquareRoot = (a, accuracy=0.0001, maxIter=1000) => {
-    if (a < 0) {
-        return NaN;
+const newtonSquareRoot = (a, accuracy = 0.0001, maxIter = 1000) => {
+  if (a < 0) {
+    return NaN;
+  }
+  if (a === 0 || a === 1) {
+    return a;
+  }
+
+  const b = a / 2;
+  let iter = 0;
+
+  const innerFunc = (num, guess) => {
+    guess = 0.5 * (guess + (num / guess));
+    iter += 1;
+
+    if (Math.abs(guess - num) < accuracy || iter === maxIter) {
+      return guess;
     }
-    if (a === 0 || a === 1) {
-        return a;
-    }
+    return innerFunc(num, guess);
+  };
 
-    b = a / 2;
-    iter = 0;
-
-    const inner_func = (a, b) => {
-        b = 0.5 * (b + a/b);
-        iter++;
-
-        if ( Math.abs(b - a) < accuracy || iter === maxIter) {
-            return b;
-        } else {
-            return inner_func(a, b);
-        }
-    }
-
-    return inner_func(a, b);  
+  return innerFunc(a, b);
 };
 
 module.exports = newtonSquareRoot;
--- test/algorithms/math/testNewtonSquareRoot.js
+++ test/algorithms/math/testNewtonSquareRoot.js
@@ -1,26 +1,28 @@
-const newtonSquareRoot = require("../../../src").algorithms.math.newtonSquareRoot;
-const assert = require("assert");
+/* eslint-env mocha */
+const newtonSquareRoot = require('../../../src').algorithms.math.newtonSquareRoot;
 
-describe("Newton's method for square root", () => {
-    it("should return 0 when the the square root of 0 is requested", () => {
-        assert.equal(newtonSquareRoot(0), 0);
-    });
+const assert = require('assert');
 
-    it("should return 1 when the the square root of 1 is requested", () => {
-        assert.equal(newtonSquareRoot(0), 0);
-    });
+describe('Newtons method for square root', () => {
+  it('should return 0 when the the square root of 0 is requested', () => {
+    assert.equal(newtonSquareRoot(0), 0);
+  });
 
-    it("should return NaN when the the square root of any negative number is requested", () => {
-        assert.equal(isNaN(newtonSquareRoot(-1)), true);
-        assert.equal(isNaN(newtonSquareRoot(-121935.346)), true);
-    });
+  it('should return 1 when the the square root of 1 is requested', () => {
+    assert.equal(newtonSquareRoot(0), 0);
+  });
 
-    it("should return an integer when the the square root of any perfect square is requested", () => {
-        assert.equal(newtonSquareRoot(25), 5);
-        assert.equal(newtonSquareRoot(998001), 999);
-    });
+  it('should return NaN when the the square root of any negative number is requested', () => {
+    assert.equal(isNaN(newtonSquareRoot(-1)), true);
+    assert.equal(isNaN(newtonSquareRoot(-121935.346)), true);
+  });
 
-    it("The difference between the returned value and the internal javascript value should be less than 1e-5", () => {
-        assert.equal(newtonSquareRoot(2) - Math.sqrt(2) < 0.00001, true);
-    });
+  it('should return an integer when the the square root of any perfect square is requested', () => {
+    assert.equal(newtonSquareRoot(25), 5);
+    assert.equal(newtonSquareRoot(998001), 999);
+  });
+
+  it('The difference between the returned value and the internal javascript value should be less than 1e-5', () => {
+    assert.equal(newtonSquareRoot(2) - Math.sqrt(2) < 0.00001, true);
+  });
 });
