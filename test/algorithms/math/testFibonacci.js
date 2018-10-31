67c6eb537e5258aeca9159023e7ec9220966623f
Justin Kook <justinkook@gmail.com>
2018-10-31 15:17:55 -0400

    Added testFibonacci.js

---
+++ test/algorithms/math/testFibonacci.js
@@ -0,0 +1,16 @@
+const fibonacci = require('../../../src/algorithms/math/fibonacci');
+ const assert = require('assert');
+ describe('Fibonacci', () => {
+  it('returns 0 if given number is 0', () => {
+    assert.deepStrictEqual(fibonacci(0), [0]);
+  });
+   it('if the number is negative, returns an error', () => {
+    assert.throws(() => fibonacci(-5));
+  });
+   it('returns a fibonacci sequence up to 10', () => {
+    assert.deepStrictEqual(fibonacci(10), [0, 1, 1, 2, 3, 5, 8]);
+  });
+   it('returns fibonacci sequence up to 100', () => {
+    assert.deepStrictEqual(fibonacci(100), [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]);
+  });
+});
