fd39078a62077599999009b9305a641fb3bb0bf4
Justin Kook <justinkook@gmail.com>
2018-10-30 21:40:34 -0400

    changed fibonacci.js

--- src/algorithms/math/fibonacci.js
+++ src/algorithms/math/fibonacci.js
@@ -4,23 +4,23 @@
  * @param  {Number} a initial first number 1
  * @param  {Number} b initial second number 0
  * @return {Number} temp updated sum of two numbers (a and b)
- * 
  * References: https://medium.com/developers-writing/fibonacci-sequence-algorithm-in-javascript-b253dc7e320e
  */
 
 const fibonacci = (num) => {
-    let a = 1,
-      b = 0,
-      temp;
-
-    while (num >= 0) {
-      temp = a;
-      a = a + b;
-      b = temp;
-      num--;
-    }
-
-    return b;
-  }
+  let a = 0;
+  let b = 1;
+  const temp = [];
+   if (num < 0) {
+    throw new Error('Not a positive number');
+  };
+   while (a <= num) {
+    temp.push(a);
+     const sum = a + b;
+    a = b;
+    b = sum;
+  };
+   return temp;
+};
 
 module.exports = fibonacci;
