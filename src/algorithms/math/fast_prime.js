f51332b4eed9ac480a060a76c6f1290e453066a3
mohamedveron <mohamedveron23@gmail.com>
2018-11-03 15:53:28 +0200

    optimezied prime checking algorithm added

---
+++ src/algorithms/math/fast_prime.js
@@ -0,0 +1,17 @@
+/**
+ * Check if number is prime or not 
+ * @param  {Number} a number to be checked
+ * @return {Number}   true or false
+ */
+const isPrime = (a) => {
+  
+  for(let i = 2; i <= Math.sqrt(a); i++){
+      if(a % i == 0){
+          return false;
+      }
+  }
+
+  return (a > 1) ? true : false; 
+};
+
+module.exports = isPrime;
