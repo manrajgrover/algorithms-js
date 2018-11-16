bb29144e1b268ac5083bac3b81b72c4c90237e73
mohamedveron <mohamedveron23@gmail.com>
2018-11-16 22:51:31 +0200

    quality code updated

--- src/algorithms/math/fast_prime.js
+++ src/algorithms/math/fast_prime.js
@@ -4,13 +4,13 @@
 * @return {Number}  true or false
 */
 const isPrime = (a) => {
-  for (let i = 2; i <= Math.sqrt(a); i++){
+  for (let i = 2; i <= Math.sqrt(a); i++) {
     if (a % i === 0) {
-        return false;
+      return false;
     }
   }
 
-  return (a > 1) ? true : false; 
+return (a > 1) ? true : false; 
 };
 
 module.exports = isPrime;
--- test/algorithms/math/testFastPrime.js
+++ test/algorithms/math/testFastPrime.js
@@ -19,10 +19,10 @@ describe('Fast Prime Number checking', () => {
     assert.equal(fastPrime(953), true);
   });
 
-   it('should solve for big numbers  ', () => {
-        assert.equal(fastPrime(999917), true);
-        assert.equal(fastPrime(1000000), false);
-        assert.equal(fastPrime(9999973), true);
-        assert.equal(fastPrime(10000000), false);
-   });
+  it('should solve for big numbers  ', () => {
+      assert.equal(fastPrime(999917), true);
+      assert.equal(fastPrime(1000000), false);
+      assert.equal(fastPrime(9999973), true);
+      assert.equal(fastPrime(10000000), false);
+  });
 });
