5d2b2324b3566ed6968c2b99937a8b6ddf7ae16e
Victor <vkubrak@myseneca.ca>
2018-12-09 20:08:32 -0400

    Update testFisherYates.js

--- test/algorithms/math/testFisherYates.js
+++ test/algorithms/math/testFisherYates.js
@@ -5,8 +5,8 @@ const assert = require('assert');
 
 describe('Fisher-Yates', () => {
   it('should return array with the same elements', () => {
-    var arr1 = [1,2,3,4,5];
-    var arr2 = arr1;
+    let arr1 = [1, 2, 3, 4, 5];
+    let arr2 = arr1;
     fisherYates(arr1);
     arr1.sort();
     assert.equal(arr1, arr2);
