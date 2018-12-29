33a67977e2f0cfd77301a43a3d1c10a5c92088ee
nahome <nahom.teshome@yahoo.com>
2018-12-28 23:44:36 -0800

    valid-traingle complete

--- src/algorithms/geometry/valid_traingle.js
+++ src/algorithms/geometry/valid_traingle.js
@@ -2,7 +2,7 @@ function validTraingle(a, b, c) {
   if (a + b <= c || b + c <= a || a + c <= b) {
     return false;
   }
-    return true;
+  return true;
 }
 
 module.exports = validTraingle;
--- test/algorithms/geometry/testValidTraingle.js
+++ test/algorithms/geometry/testValidTraingle.js
@@ -1,18 +1,18 @@
+/* eslint-env mocha */
 const assert = require('assert');
 
 const validTraingle = require('../../../src/algorithms/geometry/valid_traingle');
 
-describe('validTraingle', function() {
-  it('should return false if the sum of two sides is less than or equal to the third', function () {
-    assert.equal(false, validTraingle(1,2,3));
-    assert.equal(false, validTraingle(3,6,9));
+describe('validTraingle', () => {
+  it('should return false if the sum of two sides is less than or equal to the third', () => {
+    assert.equal(false, validTraingle(1, 2, 3));
+    assert.equal(false, validTraingle(3, 6, 9));
     assert.equal(false, validTraingle(5, 10, 16));
   });
 
-  it('should return true if the sum of two sides is always greater than the third', function () {
+  it('should return true if the sum of two sides is always greater than the third', () => {
     assert.equal(true, validTraingle(3, 4, 5));
     assert.equal(true, validTraingle(5, 6, 9));
     assert.equal(true, validTraingle(3, 2, 4));
   });
-
 });
