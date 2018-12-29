85faca4fbae6c84658b0e1177a56f5af6791a8a0
nahome <nahom.teshome@yahoo.com>
2018-12-28 20:13:36 -0800

    binary search recursive fix

--- src/algorithms/geometry/index.js
+++ src/algorithms/geometry/index.js
@@ -1,7 +1,5 @@
 const CircleTangents = require('./tangent_between_circles');
-var validTraingle = require('./valid_traingle');
 
 module.exports = {
-  CircleTangents,
-  validTraingle
+  CircleTangents
 };
--- src/algorithms/search/binary_search_recursive.js
+++ src/algorithms/search/binary_search_recursive.js
@@ -5,35 +5,21 @@
  * @return {Number}             Index of the element, if found
  */
 const binarysearchRecursive = (array, low, high, element) => {
-  var mid;
-console.log(low,high);
-//base condition
-if(low > high)
-{
-  return -1;
+  let mid;
+
+  if (low > high) {
+    return -1;
 }
 
-//calculate the midpoint of array
-mid=Math.floor(low+(high-low)/2);
-console.log(mid);
+    mid = Math.floor(low + (high - low) / 2);
 
-if (element == array[mid])
-{
-  return mid;
+  if (element === array[mid]) {
+    return mid;
+} else if (element < array[mid]) {
+    return binarysearchRecursive(array, low, mid - 1, element);
+} else {
+    return binarysearchRecursive(array, mid + 1, high, element);
 }
-
-else if (element<array[mid])
-{
-  //if the number is less the number at the midpoint of array
-  return binarysearchRecursive(array,low,mid-1,element);
-}
-
-else
-{
-  //if the number is more the number at the midpoint of array
-  return binarysearchRecursive(array,mid+1,high,element);
-}
-
 };
 
 module.exports = binarysearchRecursive;
