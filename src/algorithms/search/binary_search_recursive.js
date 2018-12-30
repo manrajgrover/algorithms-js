07cea24da222cd88158eaebf028a5eda4ad9ed3c
nahome <nahom.teshome@yahoo.com>
2018-12-29 19:38:31 -0800

    still binary searching

--- src/algorithms/search/binary_search_recursive.js
+++ src/algorithms/search/binary_search_recursive.js
@@ -4,33 +4,23 @@
  * @param  {Number} element     Element to be searched
  * @return {Number}             Index of the element, if found
  */
- function binarysearchRecursive(array, low, high, num) {
-   //let mid;
-   console.log(low, high);
-   //base condition
-   if(low > high) {
-     return -1;
+function binarysearchRecursive(array, low, high, num) {
+  console.log(low, high);
+
+  if (low > high) {
+    return -1;
+  }
+
+  const midPoint = (high - low) / 2;
+  let mid = Math.floor(low + midPoint);
+
+  if (num === array[mid]) {
+   return mid;
+  } else if (num < array[mid]) {
+
+      return binarysearchRecursive(array, low, mid - 1, num);
    }
-
-   //calculate the midpoint of array
-   const midPoint = (high - low) / 2;
-   let mid = Math.floor(low + midPoint);
-   console.log(mid);
-
-   if (num === array[mid]) {
-   	return mid;
-   }
-
-   else if (num < array[mid]) {
-   	//if the number is less the number at the midpoint of array
-   	return binarysearchRecursive(array, low, mid-1, num);
-   }
-
-   else {
-   	//if the number is more the number at the midpoint of array
-   	return binarysearchRecursive(array, mid+1, high, num);
-   }
-
- }
+      return binarysearchRecursive(array, mid + 1, high, num);
+}
 
 module.exports = binarysearchRecursive;
