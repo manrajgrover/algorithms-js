4c7ff88ef0e05c92ef4531ab2606de3436ef7ba0
mohamedveron <mohamedveron23@gmail.com>
2019-07-11 22:25:35 +0200

    new changes added

--- src/algorithms/math/fast_prime.js
+++ src/algorithms/math/fast_prime.js
@@ -4,14 +4,17 @@
 * @return {Number}  true or false
 */
 const isPrime = (a) => {
-  for (let i = 2; i <= Math.sqrt(a); i+= 1) {
+
+  // because 0, 1 and any even number > 2 are not primes
+  if (a === 1 || a === 0 || (a > 2 && a % 2 === 0)) {
+    return false;
+  }
+
+  const root = Math.sqrt(a); // cashe root
+  for (let i = 3; i <= root; i+= 2) {
     if (a % i === 0) {
       return false;
     }
-  }
-
-  if (a === 1 || a === 0) {
-    return false;
   }
 
   return true;
