efae8922471a7b51923240a64760474ab3ec1bf0
gnsp <sir.gnsp@gmail.com>
2019-10-01 17:31:50 +0530

    add modular combination

---
+++ src/algorithms/math/modular_combination.js
@@ -0,0 +1,26 @@
+const fermetModularInverse = require('./modular_inverse').fermetModularInverse;
+
+/**
+ * Calculates modular combination (n, r, p), i.e. Binomial coefficient (n, r) modulo p,
+ * using Fermat's little theorem
+ *
+ * @param  {Number} n Number of items in collection
+ * @param  {Number} r Number of items in selection
+ * @param  {Number} p Mod val
+ * @return {Number}   Modular Combination
+ */
+const modularCombination = (n, r, p) => {
+  if (r > n || p == 0) return null;
+
+  let num = 1;
+  let den = 1;
+
+  for (let i=0; i<r; i++) {
+    num = (num * (n - i)) % p;
+    den = (den * (i + 1)) % p;
+  }
+
+  return (num * fermetModularInverse(den, p)) % p;
+}
+
+module.exports = modularCombination;
