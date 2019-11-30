04ff82e970f2ba95cbe2b76c1abd9fdbd8cb0348
Bobbay <49123594+Bobbbay@users.noreply.github.com>
2019-11-29 22:04:04 -0500

    Update testLCS.js

--- test/algorithms/string/testLCS.js
+++ test/algorithms/string/testLCS.js
@@ -1,34 +1,11 @@
-const LCS = require('lcs');
+const LCS = require('../../../src').algorithms.string.longestCommon;
 
-describe('Levenshtein Distance', () => {
-  it('should find levenshtein distance between two strings', () => {
-    const stringA = 'sunday';
-    const stringB = 'saturday';
+describe('Longest Common', () => {
+  it('should find longest common substring', () => {
+    const stringA = 'testyyyfoo';
+    const stringB = 'bazzyyfoobaazz';
 
-    const distance = levenshteindistance(stringA, stringB);
-    assert.equal(distance, 3);
-  });
-
-  it('should return length of string A when B is empty', () => {
-    const stringA = 'sunday';
-    const stringB = '';
-
-    const distance = levenshteindistance(stringA, stringB);
-    assert.equal(distance, 6);
-  });
-
-  it('should return length of string B when A is empty', () => {
-    const stringA = '';
-    const stringB = 'saturday';
-
-    const distance = levenshteindistance(stringA, stringB);
-    assert.equal(distance, 8);
-  });
-
-  it('should return distance as 0 when no params are provided', () => {
-    const distance = levenshteindistance();
-    assert.equal(distance, 0);
+    const distance = longestCommon(stringA, stringB);
+    assert.equal(distance, "foo");
   });
 });
-
-//alert(longestCommon("lalulalulu", "laalulu"));
