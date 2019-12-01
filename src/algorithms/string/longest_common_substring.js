32ccb490529f2354496fd6fed004461230f5a251
Bobbay <49123594+Bobbbay@users.noreply.github.com>
2019-11-30 20:57:30 -0500

    Update longest_common_substring.js

--- src/algorithms/string/longest_common_substring.js
+++ src/algorithms/string/longest_common_substring.js
@@ -1,100 +1,24 @@
-// GENERIC FUNCTIONS ----------------------------
+function lcs(lcstest, lcstarget) {
+ matchfound = 0
+ lsclen = lcstest.length
+  for(lcsi=0; lcsi<lcstest.length; lcsi++){
+   lscos=0
+    for(lcsj=0; lcsj<lcsi+1; lcsj++){
+     re = new RegExp("(?:.{" + lscos + "})(.{" + lsclen + "})", "i");
+     temp = re.test(lcstest);
+     re = new RegExp("(" + RegExp.$1 + ")", "i");
+      if(re.test(lcstarget)){
+       matchfound=1;
+       result = RegExp.$1;
+       break;
+       }
+     lscos = lscos + 1;
+     }
+     if(matchfound==1){return result; break;}
+    lsclen = lsclen - 1;
+   }
+  result = "";
+  return result;
+ }
 
-// Each member of a list of functions applied to each
-// of a list of arguments, deriving a list of new values.
-
-// apList (<*>) :: [(a -> b)] -> [a] -> [b]
-const apList = (fs, xs) => //
-    fs.reduce((a, f) => a.concat(
-        xs.reduce((a, x) => a.concat([f(x)]), [])
-    ), []);
-
-// comparing :: (a -> b) -> (a -> a -> Ordering)
-const comparing = f =>
-    (x, y) => {
-        const
-            a = f(x),
-            b = f(y);
-        return a < b ? -1 : (a > b ? 1 : 0);
-    };
-
-// compose (<<<) :: (b -> c) -> (a -> b) -> a -> c
-const compose = (f, g) => x => f(g(x));
-
-// concat :: [[a]] -> [a]
-// concat :: [String] -> String
-const concat = xs =>
-    0 < xs.length ? (() => {
-        const unit = 'string' !== typeof xs[0] ? (
-            []
-        ) : '';
-        return unit.concat.apply(unit, xs);
-    })() : [];
-
-// concatMap :: (a -> [b]) -> [a] -> [b]
-const concatMap = (f, xs) =>
-    xs.reduce((a, x) => a.concat(f(x)), []);
-
-// inits([1, 2, 3]) -> [[], [1], [1, 2], [1, 2, 3]
-// inits('abc') -> ["", "a", "ab", "abc"]
-
-// inits :: [a] -> [[a]]
-// inits :: String -> [String]
-const inits = xs => [
-        []
-    ]
-    .concat(('string' === typeof xs ? xs.split('') : xs)
-        .map((_, i, lst) => lst.slice(0, i + 1)));
-
-// intersect :: (Eq a) => [a] -> [a] -> [a]
-const intersect = (xs, ys) =>
-    xs.filter(x => -1 !== ys.indexOf(x));
-
-// Returns Infinity over objects without finite length.
-// This enables zip and zipWith to choose the shorter
-// argument when one is non-finite, like cycle, repeat etc
-
-// length :: [a] -> Int
-const length = xs =>
-    (Array.isArray(xs) || 'string' === typeof xs) ? (
-        xs.length
-    ) : Infinity;
-
-// map :: (a -> b) -> [a] -> [b]
-const map = (f, xs) => xs.map(f);
-
-// maximumBy :: (a -> a -> Ordering) -> [a] -> a
-const maximumBy = (f, xs) =>
-    0 < xs.length ? (
-        xs.slice(1)
-        .reduce((a, x) => 0 < f(x, a) ? x : a, xs[0])
-    ) : undefined;
-
-// tail :: [a] -> [a]
-const tail = xs => 0 < xs.length ? xs.slice(1) : [];
-
-// tails :: [a] -> [[a]]
-const tails = xs => {
-    const
-        es = ('string' === typeof xs) ? (
-            xs.split('')
-        ) : xs;
-    return es.map((_, i) => es.slice(i))
-        .concat([
-            []
-        ]);
-};
-
-// longestCommon :: String -> String -> String
-const longestCommon = (s1, s2) => maximumBy(
-    comparing(length),
-    intersect(...apList(
-        [s => map(
-            concat,
-            concatMap(tails, compose(tail, inits)(s))
-        )],
-        [s1, s2]
-    ))
-);
-
-module.exports = longestCommon;
+module.exports = lcs;
