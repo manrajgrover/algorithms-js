d77ffc38f4dcbeb744eca63eb5f156800592ec16
Hunter Gayden <gayden.hunter@gmail.com>
2020-08-07 17:08:08 -0400

    Close #25: Implement Viterbi algo and fix JSDocs

--- src/algorithms/optimization/viterbi.js
+++ src/algorithms/optimization/viterbi.js
@@ -10,7 +10,7 @@
  * @typedef {any} Observation
  */
 /**
- * The set of all possible observations of the system
+ * An unordered list of all possible observations of the system
  * @typedef {Observation[]} ObservationSpace
  */
 
@@ -19,28 +19,99 @@
  * @typedef {any} State
  */
 /**
- * The set of all possible hidden states
+ * An unordered list of all possible hidden states
  * @typedef {State[]} StateSpace
  */
 
 /**
- * A 2D matrix, A, of size K x K (where K is the number of states in the state space) such that A[i][j] gives the probability of a transition from state i to state j
- * @typedef {Object<String, Number[]>} TransitionMatrix
+ * A nested map such that two state names in order gives the probability of a transition from the first to the second:
+ *  map.name1.name2 => probability of transition from state 1 to state 2
+ * @typedef {Object<String, Object<String, Number>>} TransitionMap
  */
 /**
- * A 2D matrix, B, of size K x N (where K is the number of states in the state space and N is the number of observations in the observation space) such that B[i][j] gives the probability of observation j resulting from state i
- * @typedef {Object<String, Number[]>} EmissionMatrix
+ * A nested map such that a state name followed by an observation name gives the probability of that observation resulting from that state:
+ *  map.stateName.obsName => probability of named state leading to named observation
+ * @typedef {Object<String, Object<String, Number>>} EmissionMap
  */
 
 /**
  * Determine the Viterbi Path of a given set of Observations
  *
- * @param  {ObservationSpace} O - observation space
- * @param  {StateSpace} S - state space
- * @param  {Object<String, Number>} P - an array that, for each state in S, gives the probability that the initial hidden state is that state
+ * @param  {ObservationSpace} O
+ * @param  {StateSpace} S
+ * @param  {Object<String, Number>} P0 - a map which gives the probability that each state in S is the initial hidden state
  * @param  {Observation[]} Y - the sequence of recorded observations for which the Viterbi Path is to be found
- * @param  {TransitionMatrix} A - a "transition matrix" of [ length(StSp) X length(StSp) ] numbers giving the probability of transition from every state to every other state
- * @param  {EmissionMatrix} B - an "emission matrix" of [ length(StSp) X length(ObsSp) ] numbers giving the probability of every possible observation for every possible state
+ * @param  {TransitionMap} A
+ * @param  {EmissionMap} B
  *
  * @return {State[]} (denoted X) the most likely sequence of (hidden) states
  */
+function viterbi(O, S, P0, Y, A, B) {
+  /** probability of the state with greatest likelihood at each observation, given the previous state
+   *
+   * the innermost (nested) arrays give probabilities at each observation in sequence; each member corresponds with a successive observation
+   * the outermost (container) array enumerates all possible probability sequences; each member assumes the unique initial state at the same index in S
+   * @type Number[][]
+   */
+  const T1 = [];
+
+  /** state (with corresponding probability in T1) with greatest likelihood at each observation, given the previous state
+   * @type State[][]
+   */
+  const T2 = [];
+
+  //  Calculate the probability of each initial state
+  //  These are irrespective of any observations
+  for (let i = 0; i < S.length; ++i) {
+    T1[i] = [ P0[ S[i] ] * B[ S[i] ][ Y[0] ] ];
+    T2[i] = [ null ];
+  }
+
+  //  determine the probability of each state state underlying each observation
+  //  the calculations account for the current observation the probability of
+  //  the path leading to the previous most likely state
+  for (let j = 1; j < Y.length; ++j) { //  for each observation (in sequence)
+    for (let i = 0; i < S.length; ++i) { //  find the probability of every possible state
+      let Pmax = -1; //  guarantee inner conditional satisfied on first iteration
+      let kPmax;
+      let k = 0;
+      do {
+        let p = T1[k][j - 1] * A[ S[k] ][ S[i] ] * B[ S[i] ][ Y[j] ];
+        if (p > Pmax) {
+          Pmax = p;
+          kPmax = k;
+        }
+      } while (++k < S.length);
+      T1[i][j] = Pmax;
+      T2[i][j] = kPmax;
+    }
+  }
+
+  //  choose most likely path from T1
+  const T = Y.length;
+  const Z = []; // indices
+  const X = []; // states
+
+  //  determine final observed state
+  Z[T - 1] = T2[0][T - 1]; //  initialize to known value
+  X[T - 1] = S[Z[T - 1]];
+  for (let i = 1; i < S.length; ++i) { // skip the value used to init Z[T - 1]
+    if (T1[i][T - 1] > T1[Z[T - 1]][T - 1]) {
+      Z[T - 1] = i;
+      X[T - 1] = S[i];
+    }
+  }
+
+  //  determine Z and X in reverse order
+  for (let j = T - 1; j > 0; --j) {
+    Z[j - 1] = T2[Z[j]][j];
+    X[j - 1] = S[Z[j - 1]];
+  }
+
+  return X;
+}
+
+
+//  function to compose a transition state matrix from a MarkovChain
+
+module.exports = viterbi;
