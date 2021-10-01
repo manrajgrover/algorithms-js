5e111adc2831e0ad68edea7689a658fa896e5a52
Shreyas Shrawage <shreyas.shrawage2000@gmail.com>
2021-10-01 14:57:57 +0530

    Added more test cases

--- test/algorithms/math/testGcdMultipleNumbers.js
+++ test/algorithms/math/testGcdMultipleNumbers.js
@@ -5,9 +5,9 @@ const gcd_multiple_numbers =
 const assert = require("assert");
 
 describe("GCD", () => {
-  it("should return 0 for either or both numbers as 0", () => {
+  it("should return 0 for either or the numbers as 0", () => {
     assert.equal(gcd_multiple_numbers(0, 10), 0);
-    assert.equal(gcd_multiple_numbers(10, 0), 0);
+    assert.equal(gcd_multiple_numbers(10, 0, 1), 0);
     assert.equal(gcd_multiple_numbers(0, 0), 0);
   });
 
@@ -23,9 +23,16 @@ describe("GCD", () => {
     assert.equal(gcd_multiple_numbers(3, 2), 1);
   });
 
+  it("should return gcd of multiple numbers", () => {
+    assert.equal(gcd_multiple_numbers(60, 10, 5), 5);
+    assert.equal(gcd_multiple_numbers(33, 10, 4), 1);
+    assert.equal(gcd_multiple_numbers(3, 6, 9, 12, 18), 3);
+  });
+
   it("should return gcd of negative numbers", () => {
     assert.equal(gcd_multiple_numbers(-60, -10), 10);
     assert.equal(gcd_multiple_numbers(65, -10), 5);
+    assert.equal(gcd_multiple_numbers(65, -10, -3), 1);
     assert.equal(gcd_multiple_numbers(-3, 2), 1);
   });
 });
