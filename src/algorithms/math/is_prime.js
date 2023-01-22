51d06e18f0c782a6a584c252324b5b702910c509
Francesca Calla Bruc <cheskabrucee@gmail.com>
2023-01-22 12:46:34 -0800

    added is_prime function

---
+++ src/algorithms/math/is_prime.js
@@ -0,0 +1,17 @@
+/**
+ * Checks if a num is prime or not
+ * @param  {Number} num number to check
+ * @return {Boolean} true if num is prime, false if num is not prime
+ *
+ * References: https://javascript.plainenglish.io/11-mathematical-algorithms-in-modern-javascript-bce71318e2da
+ */
+ const isprime = num => {
+    const limit = Math.floor(Math.sqrt(num));
+    for (let i = 2; i <= limit; i++){ 
+        if (n % i === 0) return false;
+    }
+     return n >= 2;
+  };
+  
+  module.exports = isprime;
+  
