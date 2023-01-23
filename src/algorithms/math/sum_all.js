ae6c58d45b76951cd196921086008f71a95a2e18
Francesca Calla Bruc <cheskabrucee@gmail.com>
2023-01-22 18:33:46 -0800

    added basic functions

---
+++ src/algorithms/math/sum_all.js
@@ -0,0 +1,17 @@
+/**
+ * Calculates the sum of all numbers in an array
+ * @param  {Array} arr of two ints where arr[0] start, arr[1] end
+ * @return {Number}  sum sum of the range of numberse
+ */
+const sumall = (arr) => {
+    var sum =0;
+    var min = arr[0];
+    var max = arr[1];
+    for (var i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
+};
+
+module.exports = sumall;
