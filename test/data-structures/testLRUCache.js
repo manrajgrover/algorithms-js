25dc8b14bf22cc5974ec75bbc0202b04151f2d7f
zhengjynicolas <zhengjynicolas@hotmail.com>
2023-11-19 14:44:27 +0800

    update test cases for LRUCache

--- test/data-structures/testLRUCache.js
+++ test/data-structures/testLRUCache.js
@@ -13,45 +13,36 @@ describe('assert LRUCache', () => {
   it('store.cache(\'b\', 2), expect store[\'b\'] to be 2', () => {
     assert.equal(store.cache('b', 2)['b'], 2);
   });
-  it('set store.a = 5, expect store.a to be 5', () => {
+  it('store.a = 5, expect store.a to be 5', () => {
     store.a = 5;
     assert.equal(store.a, 5);
   });
-  it('store.b to be undefined, store.c to be 3, store.d to be 4, store.a to be 5, store.d to be 4 store.size to be 3', () => {
+  it('store.cache(\'c\', 3).cache(\'d\', 4), store.b to be undefined, store.c to be 3, store.d to be 4, store.a to be 5, store.d to be 4 store.size to be 3', () => {
     store.cache('c', 3).cache('d', 4);
-    assert.equal(store.b, undefined, 'store.b');
-    assert.equal(store.c, 3, 'store.c');
-    assert.equal(store.a, 5, 'store.a');
-    assert.equal(store.d, 4, 'store.d');
-    assert.equal(store.size, 3, 'store.size');
+    assert.equal(store.b, undefined);
+    assert.equal(store.c, 3);
+    assert.equal(store.a, 5);
+    assert.equal(store.d, 4);
+    assert.equal(store.size, 3);
   });
   it('store.delete(\'delete\', expect return to be false)', () => {
     assert.equal(store.delete('delete'), false);
   });
   it('store.delete(\'d\'), expect return to be true, store.d to be undefined', () => {
     assert.equal(store.delete('d'), true);
-    assert.equal(store.d, undefined, 'store.d');
+    assert.equal(store.d, undefined);
   });
   it('store.delete(\'e\'), expect return to be true, store.size to be 2', () => {
-    assert.equal(store.delete('e'), true, "store.delete('e')");
-    assert.equal(store.size, 2, 'store.size');
+    assert.equal(store.delete('e'), true);
+    assert.equal(store.size, 2);
   });
-  it('store.c should be 4', () => {
+  it('store.cache(\'c\', 4), store.c should be 4', () => {
     store.cache('c', 4);
     assert.equal(store.c, 4);
   });
-  it('store.c should be 4', () => {
-    store.cache('c', 4);
-    assert.equal(store.c, 4);
-  });
-  it('store.length should be 1', () => {
+  it('store.capacity = 1, store.length should be 1, store[0] should be \'c\'', () => {
     store.capacity = 1;
     assert.equal(Object.keys(store).length, 1);
-  });
-  it('store.length should be 1, store[0] should be "c"', () => {
-    assert.equal(Object.keys(store).length, 1);
-  });
-  it('store[0] should be "c"', () => {
     assert.equal(Object.keys(store)[0], 'c');
   });
 });
